3f4bf2c77b0315626809f889476b4be0624e8a3d
Budkovsky <piotr.budek@gmail.com>
2020-02-07 16:10:09 +0100

    shell scripts update

--- bin/container-prune.sh
+++ bin/container-prune.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-docker container stop dsig-php71 && docker rm dsig-php71 && docker rmi dsig-php71
+source bin/vars.sh
+
+echo ${CONTAINER_NAME}
+
+if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ] 
+then
+#stop container if running
+	docker stop ${CONTAINER_NAME}
+fi
+
+if [ "$(docker ps -qa -f name=${CONTAINER_NAME})" ] 
+then
+#remove container, if exists
+	docker rm ${CONTAINER_NAME} $@
+fi
+
+if [ "$(docker image ls ${CONTAINER_NAME})" ] 
+then
+#remove image, if exists
+	docker rmi ${CONTAINER_NAME}
+fi
--- bin/container-run.sh
+++ bin/container-run.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-docker run -t -d -v //git/dsig-xml-builder://app -w //app --name=dsig-php71 dsig-php71
+source bin/vars.sh
+
+if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ] 
+then
+#container is running, nothing to do
+	exit 0
+fi
+
+if [ "$(docker ps -qa -f name=${CONTAINER_NAME})" ]
+then
+	#container stopped, start needed only
+	docker start ${CONTAINER_NAME}
+	exit 0
+fi
+
+if [ ! "$(docker image ls ${CONTAINER_NAME})" ]
+then
+	docker build -t ${CONTAINER_NAME} $(pwd)/${DOCKERFILE_PATH}
+fi 
+
+#run container 
+docker run -t -d -v /$(pwd)://app -w //app --name=${CONTAINER_NAME} ${CONTAINER_NAME}
+exit 0
